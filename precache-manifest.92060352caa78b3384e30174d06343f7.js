6fe832c616c87c96fa97506d68b8e99c1fc58ab4
Rajatm544 <rajatm544@gmail.com>
2020-06-28 20:25:37 +0530

    Updates

---
+++ precache-manifest.92060352caa78b3384e30174d06343f7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2474407a26327fd0635e3b2f40c54d2c",
+    "url": "/react-hooks-weather-app/index.html"
+  },
+  {
+    "revision": "75fd24bef47097ef5893",
+    "url": "/react-hooks-weather-app/static/css/main.91ffb561.chunk.css"
+  },
+  {
+    "revision": "d6adc08b872dee87d467",
+    "url": "/react-hooks-weather-app/static/js/2.86203180.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-hooks-weather-app/static/js/2.86203180.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "75fd24bef47097ef5893",
+    "url": "/react-hooks-weather-app/static/js/main.89b960f5.chunk.js"
+  },
+  {
+    "revision": "07faa7a2329a359bece3",
+    "url": "/react-hooks-weather-app/static/js/runtime-main.5a51d5c5.js"
+  }
+]);
